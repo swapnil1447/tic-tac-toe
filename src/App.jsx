4bffff252c6fe3cc9a89663c65d46dd46d1d4c82
GDIINDIA\smishra <smishra@birchstreet.net>
2024-08-18 01:34:19 +0530

    added tic-tac

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./Component/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
